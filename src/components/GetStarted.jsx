4b35b3ecfc611bcb1b09921a574400e8dafae1d6
sameershaikh55 <sameermubeen4@gmail.com>
2021-06-28 14:28:29 +0500

    issues fixed

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -5,60 +5,60 @@ import start1 from "../assets/start1.webp";
 import start2 from "../assets/start2.webp";
 
 const GetStarted = () => {
-	const data = [
-		{
-			img: start1,
-			title: "Member",
-			desc: (
-				<div className="small">
-					Download the ENDALGO app <br /> to Join the groups
-				</div>
-			),
-		},
-		{
-			img: start2,
-			title: "Leader",
-			desc: (
-				<div className="small">
-					Download the ENDALGO app <br /> to Start your group
-				</div>
-			),
-		},
-	];
-	return (
-		<div>
-			<div className="groupMembers w-100 py-5">
-				<div className="page_container">
-					<div className="container-fluid">
-						<h2 className="text-center fw-bold mb-2 mb-md-4">
-							How to get started
-						</h2>
+  const data = [
+    {
+      img: start1,
+      title: "Member",
+      desc: (
+        <div className="small">
+          Download the ENDALGO app <br /> to Join the groups
+        </div>
+      ),
+    },
+    {
+      img: start2,
+      title: "Leader",
+      desc: (
+        <div className="small">
+          Download the ENDALGO app <br /> to Start your group
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <div className="groupMembers w-100 py-5">
+        <div className="page_container">
+          <div className="container-fluid">
+            <h2 className="text-center fw-bold mb-2 mb-md-4">
+              How to get started
+            </h2>
 
-						<div className="row">
-							{data.map((prev, i) => {
-								const { img, title, desc } = prev;
+            <div className="row">
+              {data.map((prev, i) => {
+                const { img, title, desc } = prev;
 
-								return (
-									<div key={i} className="col-12 col-md-6 mt-4 mt-md-0">
-										<div className="bg-white border-1 border border-blue rounded-3 d-flex align-items-center p-4">
-											<img width="24%" src={img} alt={title} />
-											<div className="ms-3">
-												<h5 className="fw-bold">{title}</h5>
-												{desc}
-												<button className="bg-transparent border-0 small mt-2 redColor">
-													Download App <MdKeyboardArrowRight />
-												</button>
-											</div>
-										</div>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                return (
+                  <div key={i} className="col-12 col-md-6 mt-4 mt-md-0">
+                    <div className="bg-white border-1 border border-blue rounded-3 d-flex align-items-center p-4">
+                      <img width="24%" src={img} alt={title} />
+                      <div className="ms-3">
+                        <h5 className="fw-bold">{title}</h5>
+                        {desc}
+                        <button className="bg-transparent border-0 small mt-2 redColor">
+                          Download App <MdKeyboardArrowRight />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GetStarted;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,85 +12,91 @@ import { BsDot } from "react-icons/bs";
 import { useHistory } from "react-router-dom";
 
 const Header = ({ ClickEvent }) => {
-	let history = useHistory();
+  let history = useHistory();
 
-	return (
-		<div className="headerContainer">
-			<div className="innerHeaderContainer">
-				<div className="container-fluid">
-					<div className="row">
-						<div className="col-12">
-							<div className="d-flex justify-content-between align-items-center">
-								<div className="pointer" onClick={() => history.push("/")}>
-									<img className="logo" src={logo} alt="logo" />
-								</div>
-								<div onClick={ClickEvent} className="d-block d-lg-none">
-									<GiHamburgerMenu fontSize="1.8rem" />
-								</div>
-								<div className="navLinks position-absolute d-none d-lg-block">
-									<ul className="list-unstyled d-flex">
-										<li className="nestedOne fw-bold me-4 position-relative">
-											<NavLink
-												to="/"
-												className="d-flex align-items-center text-dark text-decoration-none"
-											>
-												Product{" "}
-												<RiArrowDownSLine fontSize="1.4rem" className="ms-2" />
-											</NavLink>
-											<div className="position-absolute bg-white shadow py-1 px-2 nestedNav px-3 py-3">
-												<ul className="list-unstyled">
-													<li className="pointer small mb-2">Group Management</li>
-													<li className="nestedLink pointer small py-2 px-2"><BsDot /> Features</li>
-													<li className="nestedLink pointer small py-2 px-2"><BsDot /> Pricing</li>
-												</ul>
-											</div>
-										</li>
-										<li className="fw-bold me-4">
-											<NavLink
-												to="/"
-												className="text-dark text-decoration-none"
-											>
-												Why
-											</NavLink>
-										</li>
-										<li className="fw-bold me-4">
-											<NavLink
-												to="/"
-												className="text-dark text-decoration-none"
-											>
-												Community
-											</NavLink>
-										</li>
-										<li className="fw-bold me-4">
-											<NavLink
-												to="/"
-												className="text-dark text-decoration-none"
-											>
-												FAQ
-											</NavLink>
-										</li>
-										<li className="fw-bold">
-											<NavLink
-												to="/"
-												className="text-dark text-decoration-none"
-											>
-												Company
-											</NavLink>
-										</li>
-									</ul>
-								</div>
-								<div className="btnContainer d-none d-lg-block">
-									<button className="themeBtn2 py-1 px-3">
-										Download the app
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="headerContainer d-flex align-items-center w-100">
+      <div className="innerHeaderContainer w-100">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="pointer" onClick={() => history.push("/")}>
+                  <img className="logo" src={logo} alt="logo" />
+                </div>
+                <div onClick={ClickEvent} className="d-block d-lg-none">
+                  <GiHamburgerMenu fontSize="1.8rem" />
+                </div>
+                <div className="navLinks position-absolute d-none d-lg-block">
+                  <ul className="list-unstyled d-flex">
+                    <li className="nestedOne fw-bold me-4 position-relative">
+                      <NavLink
+                        to="/"
+                        className="d-flex align-items-center text-dark text-decoration-none"
+                      >
+                        Product{" "}
+                        <RiArrowDownSLine fontSize="1.4rem" className="ms-2" />
+                      </NavLink>
+                      <div className="position-absolute bg-white shadow py-1 px-2 nestedNav px-3 py-3">
+                        <ul className="list-unstyled">
+                          <li className="pointer small mb-2">
+                            Group Management
+                          </li>
+                          <li className="nestedLink pointer small py-2 px-2">
+                            <BsDot /> Features
+                          </li>
+                          <li className="nestedLink pointer small py-2 px-2">
+                            <BsDot /> Pricing
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li className="fw-bold me-4">
+                      <NavLink
+                        to="/"
+                        className="text-dark text-decoration-none"
+                      >
+                        Why
+                      </NavLink>
+                    </li>
+                    <li className="fw-bold me-4">
+                      <NavLink
+                        to="/"
+                        className="text-dark text-decoration-none"
+                      >
+                        Community
+                      </NavLink>
+                    </li>
+                    <li className="fw-bold me-4">
+                      <NavLink
+                        to="/"
+                        className="text-dark text-decoration-none"
+                      >
+                        FAQ
+                      </NavLink>
+                    </li>
+                    <li className="fw-bold">
+                      <NavLink
+                        to="/"
+                        className="text-dark text-decoration-none"
+                      >
+                        Company
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+                <div className="btnContainer d-none d-lg-block">
+                  <button className="themeBtn2 py-1 px-3">
+                    Download the app
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
