9bf519806d782ab3465e6a9a8fdee62d6c812aec
sameershaikh55 <sameermubeen4@gmail.com>
2021-07-10 14:34:26 +0500

    remaining changes have been done

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,57 +6,61 @@ import start2 from "../assets/start2.webp";
 
 const GetStarted = () => {
   const data = [
-		{
-			img: start1,
-			title: "Member",
-			desc: (
-				<p>
-					Download the ENDALGO app <br /> to Join the groups
-				</p>
-			),
-		},
-		{
-			img: start2,
-			title: "Leader",
-			desc: (
-				<p>
-					Download the ENDALGO app <br /> to Start your group
-				</p>
-			),
-		},
-	];
-	return (
-		<div>
-			<div className="groupMembers w-100">
-				<div className="page_container">
-					<div className="container-fluid">
-						<h2 className="text-center fw-bold mb-5">How to get started</h2>
+    {
+      img: start1,
+      title: "Member",
+      desc: (
+        <p>
+          Download the ENDALGO app <br /> to Join the groups
+        </p>
+      ),
+    },
+    {
+      img: start2,
+      title: "Leader",
+      desc: (
+        <p>
+          Download the ENDALGO app <br /> to Start your group
+        </p>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <div className="groupMembers w-100">
+        <div className="page_container">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-11 col-md-12 mx-auto">
+                <h2 className="text-center fw-bold mb-5">How to get started</h2>
 
-						<div className="row">
-							{data.map((prev, i) => {
-								const { img, title, desc } = prev;
+                <div className="row">
+                  {data.map((prev, i) => {
+                    const { img, title, desc } = prev;
 
-								return (
-									<div key={i} className="col-12 col-md-6 mt-4 mt-md-0">
-										<div className="groupMCont bg-white border-1 border border-blue rounded-3 d-flex align-items-center">
-											<img width="24%" src={img} alt={title} />
-											<div className="ms-3">
-												<h5 className="fw-bold">{title}</h5>
-												{desc}
-												<button className="btnHover bg-transparent border-0 small mt-2 redColor">
-													Download App <MdKeyboardArrowRight />
-												</button>
-											</div>
-										</div>
-									</div>
-								);
-							})}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                    return (
+                      <div key={i} className="col-12 col-md-6 mt-4 mt-md-0">
+                        <div className="groupMCont bg-white border-1 border border-blue rounded-3 d-flex align-items-center">
+                          <img src={img} alt={title} />
+                          <div className="ms-3">
+                            <h5 className="fw-bold">{title}</h5>
+                            {desc}
+                            <button className="btnHover bg-transparent border-0 small mt-2 redColor">
+                              Download App <MdKeyboardArrowRight />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GetStarted;
--- src/pages/GroupManagement.jsx
+++ src/pages/GroupManagement.jsx
@@ -20,141 +20,141 @@ import app_store from "../assets/app-store-btn.webp";
 import { useHooks } from "../hooks/useHooks";
 
 const GroupManagement = () => {
-	const { isOpen, OnClick } = useHooks();
+  const { isOpen, OnClick } = useHooks();
 
-	return (
-		<div>
-			<Header ClickEvent={OnClick} />
-			<Sidebar ClickEvent={OnClick} isOpen={isOpen} />
-			{/* HERO SECTION START */}
-			<div className="hero_sec groupManagement text-white">
-				<div className="page_container h-100">
-					<div className="container-fluid d-lg-flex align-items-lg-center h-100">
-						<div className="row h-100">
-							<div className="col-12 col-md-8 col-lg-5 mx-auto text-center text-lg-start align-self-center">
-								<h2 className="fw-bold">
-									Build, Manage, and Grow with Our #1 Group App
-								</h2>
-								<h6 className="lh-base mt-3 text-white">
-									Build, manage, and grow your community with our group feature.
-									Save money on managing multiple websites and apps.
-								</h6>
-								<div className="groupHeroBtn">
-									<button className="bg-white py-2 rounded-3 px-3 border border-2 border-white">
-										Try for free
-									</button>
-									<button className="bg-transparent py-2 rounded-3 px-3 border border-2 border-white ms-3 text-white">
-										See pricing
-									</button>
-								</div>
-								{/* <div className="mt-5 d-flex d-md-none justify-content-center mt-3 mb-4">
+  return (
+    <div>
+      <Header ClickEvent={OnClick} />
+      <Sidebar ClickEvent={OnClick} isOpen={isOpen} />
+      {/* HERO SECTION START */}
+      <div className="hero_sec groupManagement text-white">
+        <div className="page_container h-100">
+          <div className="container-fluid d-lg-flex align-items-lg-center h-100">
+            <div className="row h-100">
+              <div className="col-12 col-md-8 col-lg-5 mx-auto text-center text-lg-start align-self-center">
+                <h2 className="fw-bold">
+                  Build, Manage, and Grow with Our #1 Group App
+                </h2>
+                <h6 className="lh-base mt-3 text-white">
+                  Build, manage, and grow your community with our group feature.
+                  Save money on managing multiple websites and apps.
+                </h6>
+                <div className="groupHeroBtn">
+                  <button className="bg-white py-2 rounded-3 px-3 border border-2 border-white">
+                    Try for free
+                  </button>
+                  <button className="bg-transparent py-2 rounded-3 px-3 border border-2 border-white ms-3 text-white">
+                    See pricing
+                  </button>
+                </div>
+                {/* <div className="mt-5 d-flex d-md-none justify-content-center mt-3 mb-4">
 									<img src={google_play} alt="" />
 									<img src={app_store} alt="" className="ms-3" />
 								</div> */}
-							</div>
-							<div className="col-12 col-sm-8 col-lg-7 mx-auto align-self-end">
-								<img
-									className="banner"
-									src={groupManagementBanner}
-									alt="groupManagementBanner"
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* HERO SECTION END */}
+              </div>
+              <div className="col-12 col-sm-8 col-lg-7 mx-auto align-self-end">
+                <img
+                  className="banner"
+                  src={groupManagementBanner}
+                  alt="groupManagementBanner"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* HERO SECTION END */}
 
-			<OurCustomer />
+      <OurCustomer />
 
-			{/* TRUSTING START */}
-			{/* DESKTOP */}
-			<div className="page_container d-none d-md-block">
-				<div className="container-fluid text-white">
-					<div className="trusted position-relative">
-						<div className="rightSide position-absolute">
-							<h3 className="fw-bold">
-								Trusted by 12,000 Communities Sports, fitness, and outdoor
-								activities.
-							</h3>
-							<div className="mt-4 d-flex">
-								<img src={google_play} alt="" />
-								<img src={app_store} alt="" className="ms-3" />
-							</div>
-						</div>
+      {/* TRUSTING START */}
+      {/* DESKTOP */}
+      <div className="page_container d-none d-md-block">
+        <div className="container-fluid text-white">
+          <div className="trusted position-relative">
+            <div className="rightSide position-absolute">
+              <h3 className="fw-bold">
+                Trusted by 12,000 Communities Sports, fitness, and outdoor
+                activities.
+              </h3>
+              <div className="mt-4 d-flex">
+                <img src={google_play} alt="" />
+                <img src={app_store} alt="" className="ms-3" />
+              </div>
+            </div>
 
-						<div className="trustedCont position-relative">
-							<div className="innerTrustedCont text-center position-absolute text-white">
-								<div className="h3">⭐️⭐️⭐️⭐️⭐️</div>
-								<h4 className="fw-bold">4.5/5 from 1K+ Ratings</h4>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* DESKTOP */}
+            <div className="trustedCont position-relative">
+              <div className="innerTrustedCont text-center position-absolute text-white">
+                <div className="h3">⭐️⭐️⭐️⭐️⭐️</div>
+                <h4 className="fw-bold">4.5/5 from 1K+ Ratings</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* DESKTOP */}
 
-			{/* MOBILE */}
-			<div className="page_container d-block d-md-none">
-				<div className="container-fluid text-white">
-					<div className="trusted2 position-relative">
-						<div className="rightSide position-absolute">
-							<h1 className="fw-bold">
-								Trusted by 12,000 Communities Sports, fitness, and outdoor
-								activities.
-							</h1>
-							<div className="mt-4 d-flex">
-								<img src={google_play} alt="" />
-								<img src={app_store} alt="" className="ms-3" />
-							</div>
-						</div>
+      {/* MOBILE */}
+      <div className="page_container d-block d-md-none">
+        <div className="container-fluid text-white">
+          <div className="trusted2 position-relative">
+            <div className="rightSide position-absolute">
+              <h1 className="fw-bold">
+                Trusted by 12,000 Communities Sports, fitness, and outdoor
+                activities.
+              </h1>
+              <div className="mt-4 d-flex">
+                <img src={google_play} alt="" />
+                <img src={app_store} alt="" className="ms-3" />
+              </div>
+            </div>
 
-						<div className="trustedCont2">
-							<div className="innerTrustedCont text-center position-absolute text-white">
-								<div className="starIcon">⭐️⭐️⭐️⭐️⭐️</div>
-								<h4 className="fw-bold">4.5/5 from 1K+ Ratings</h4>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* MOBILE */}
-			{/* TRUSTING END */}
+            <div className="trustedCont2">
+              <div className="innerTrustedCont text-center position-absolute text-white">
+                <div className="starIcon">⭐️⭐️⭐️⭐️⭐️</div>
+                <h4 className="fw-bold">4.5/5 from 1K+ Ratings</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* MOBILE */}
+      {/* TRUSTING END */}
 
-			<CommunityLeaders />
-			<EndalgoApp />
-			<Schedulling />
-			<GroupFeatures />
-			<EmailArrive />
+      <CommunityLeaders />
+      <EndalgoApp />
+      <Schedulling />
+      <GroupFeatures />
+      <EmailArrive />
 
-			{/* GET STARTED START */}
-			<div className="getStartedCont">
-				<div className="page_container">
-					<div className="container-fluid">
-						<div className="row">
-							<div className="col-11 col-md-12 mx-auto">
-								<div className="text-center text-white">
-									<h1 className="fw-bolder">Ready to get started?</h1>
-									<h5>Free to get started. Free to create your group.</h5>
-								</div>
-								<div className="mt-5 d-flex flex-column flex-md-row justify-content-center">
-									<button className="bg-white py-2 mb-0 fw-bold rounded-3 px-5 border border-2 border-white">
-										Try for free
-									</button>
-									<button className="bg-transparent py-2 mb-0 fw-bold rounded-3 px-5 border border-2 border-white ms-0 ms-md-3 mt-3 mt-md-0 text-white">
-										See pricing
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* GET STARTED END */}
+      {/* GET STARTED START */}
+      <div className="getStartedCont">
+        <div className="page_container">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-11 col-md-12 mx-auto">
+                <div className="text-center text-white">
+                  <h1 className="fw-bolder">Ready to get started?</h1>
+                  <h5>Free to get started. Free to create your group.</h5>
+                </div>
+                <div className="mt-5 d-flex flex-column flex-md-row justify-content-center">
+                  <button className="bg-white py-2 mb-0 fw-bold rounded-3 px-5 border border-2 border-white">
+                    Try for free
+                  </button>
+                  <button className="bg-transparent py-2 mb-0 fw-bold rounded-3 px-5 border border-2 border-white ms-0 ms-md-3 mt-3 mt-md-0 text-white">
+                    See pricing
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* GET STARTED END */}
 
-			<Footer />
-		</div>
-	);
+      <Footer />
+    </div>
+  );
 };
 
 export default GroupManagement;
